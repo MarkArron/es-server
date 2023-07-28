de00e77b138487e4f6993bdefb0eaa3cf703beff
MarkArron <135515365+MarkArron@users.noreply.github.com>
2023-07-28 20:23:32 +0800

    deleted items

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,5 @@
 const router = (app) => {
-  app.use("/books", require("./Books"));
-  app.use("/pets", require("./Pets"));
   app.use("/persons", require("./Persons"));
-  app.use("/tasks", require("./Tasks"));
 };
 
 module.exports = router;
