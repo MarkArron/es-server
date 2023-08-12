cdf036769b0cb48700ae844871be71aa852bfbd1
MarkArron <135515365+MarkArron@users.noreply.github.com>
2023-08-12 09:25:29 +0800

    statistics endpoint

--- routes/Scores.js
+++ routes/Scores.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const middleware = require("../middleware/jwt");
-const { save, find, browse } = require("../controllers/Scores");
+const { save, find, browse, statistics } = require("../controllers/Scores");
 
 router
   .post("/save", middleware, save)
   .get("/find", middleware, find)
-  .get("/browse", middleware, browse);
+  .get("/browse", middleware, browse)
+  .get("/statistics", middleware, statistics);
 
 module.exports = router;
