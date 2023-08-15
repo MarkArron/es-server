b9ab1659b0b046864e0f99e07d6ab2fd54d7ef87
MarkArron <135515365+MarkArron@users.noreply.github.com>
2023-08-15 12:52:40 +0800

    scores
    routes updates

--- routes/Scores.js
+++ routes/Scores.js
@@ -1,11 +1,18 @@
 const router = require("express").Router();
 const middleware = require("../middleware/jwt");
-const { save, find, browse, statistics } = require("../controllers/Scores");
+const {
+  save,
+  find,
+  browse,
+  statistics,
+  statistics2,
+} = require("../controllers/Scores");
 
 router
   .post("/save", middleware, save)
   .get("/find", middleware, find)
   .get("/browse", middleware, browse)
-  .get("/statistics", middleware, statistics);
+  .get("/statistics", middleware, statistics)
+  .get("/statistics2", middleware, statistics2);
 
 module.exports = router;
