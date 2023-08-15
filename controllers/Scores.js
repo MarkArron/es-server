1376055c069137d74bd49ca038710e730582b38b
MarkArron <135515365+MarkArron@users.noreply.github.com>
2023-08-15 12:53:18 +0800

    scores
    controller

--- controllers/Scores.js
+++ controllers/Scores.js
@@ -1,4 +1,6 @@
 const Scores = require("../models/Scores");
+const Persons = require("../models/Persons");
+const Exams = require("../models/Exams");
 
 exports.save = (req, res) =>
   Scores.create(req.body)
@@ -26,10 +28,94 @@ exports.find = (req, res) =>
   Scores.find(req.query)
     .populate("examinee", "email")
     .populate("exam", "title")
-    .then((examinee) => {
+    .then((examinees) => {
       res.status(201).json({
         success: "Scores found successfully",
-        payload: examinee,
+        payload: examinees,
       });
+    })
+    .catch((err) => res.status(400).json({ error: err.message }));
+
+exports.statistics = (req, res) =>
+  Scores.find()
+    .populate("examinee", "email")
+    .populate("exam", "title")
+    .then((scores) => {
+      const exams = Array.from(new Set(scores.map((score) => score.exam)));
+
+      const passingPercentages = exams.map((exam) => {
+        const passedExaminees = scores.filter(
+          (score) => score.score >= score.bank * 0.7 && score.exam?.equals(exam)
+        );
+
+        const allExaminees = scores.filter((score) => score.exam?.equals(exam));
+
+        const passingPercentage =
+          (passedExaminees.length / allExaminees.length) * 100;
+
+        return {
+          exam: exam.title,
+          percentage: passingPercentage,
+        };
+      });
+
+      res.status(201).json({
+        success: "Passing percentages per exam calculated successfully",
+        payload: passingPercentages
+          .sort((a, b) => b.percentage - a.percentage)
+          .slice(0, 10),
+      });
+    })
+    .catch((err) => res.status(400).json({ error: err.message }));
+
+exports.statistics2 = (req, res) =>
+  Scores.find(req.query)
+    .populate("examinee", "email")
+    .populate("exam", "title")
+    .then((examineeScores) => {
+      Scores.find()
+        .populate("exam", "title")
+        .then((scores) => {
+          const exams = Array.from(new Set(scores.map((score) => score.exam)));
+
+          const examineeExams = exams.map((exam) => {
+            const passedExaminees = scores.filter(
+              (score) =>
+                score.score >= score.bank * 0.7 && score.exam?.equals(exam)
+            );
+
+            const allExaminees = scores.filter((score) =>
+              score.exam?.equals(exam)
+            );
+
+            const passingPercentage =
+              (passedExaminees.length / allExaminees.length) * 100;
+
+            const matchingExamineeScore = examineeScores.find((examineeScore) =>
+              examineeScore.exam.equals(exam)
+            );
+
+            if (matchingExamineeScore) {
+              return {
+                exam: matchingExamineeScore.exam.title,
+                percentage: Math.round(passingPercentage),
+              };
+            }
+
+            return null;
+          });
+
+          const filteredExamineeExams = examineeExams.filter(
+            (examineeExam) => examineeExam !== null
+          );
+
+          res.status(201).json({
+            success: "Scores found successfully",
+            payload: filteredExamineeExams
+              .sort((a, b) => b.percentage - a.percentage)
+              .slice(0, 10),
+          });
+        })
+        .catch((err) => res.status(400).json({ error: err.message }));
     })
     .catch((err) => res.status(400).json({ error: err.message }));
