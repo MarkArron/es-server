ee1b77143cf3bea8fb8f58cab025de4baf03a230
MarkArron <135515365+MarkArron@users.noreply.github.com>
2023-08-18 12:38:20 +0800

    appointments model

---
+++ models/Appointments.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema(
+  {
+    examinee: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Persons",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ["Pending", "Approved", "Denied", "Completed"],
+        message: "Please choose a valid status from the predefined options.",
+      },
+    },
+    reason: {
+      type: string,
+      required: function () {
+        return this.status === "Denied";
+      },
+    },
+    schedule: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Schedules",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Appointments", schema);
