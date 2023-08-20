4980c40a0902fa17b50dbf93a8ae5dd71952d047
MarkArron <135515365+MarkArron@users.noreply.github.com>
2023-08-20 15:33:07 +0800

    Schedules

--- controllers/Schedules.js
+++ controllers/Schedules.js
@@ -1,6 +1,6 @@
 const Schedules = require("../models/Schedules");
 
-exports.save = (req, res) => {
+exports.save = (req, res) =>
   Schedules.create(req.body)
     .then((schedule) => {
       res.status(201).json({
@@ -9,7 +9,6 @@ exports.save = (req, res) => {
       });
     })
     .catch((err) => res.status(400).json({ error: err.message }));
-};
 
 exports.browse = (req, res) =>
   Schedules.find()
@@ -21,11 +20,23 @@ exports.browse = (req, res) =>
     )
     .catch((err) => res.status(400).json({ error: err.message }));
 
-exports.update = (req, res) => {
+exports.update = (req, res) =>
   Schedules.findByIdAndUpdate(req.body._id, req.body, { new: true })
     .then((schedule) => {
       res.json({
         success: "Schedule updated successfully",
+        payload: schedule,
+      });
+    })
+    .catch((err) => res.status(400).json({ error: err.message }));
+
+exports.exam_schedule = (req, res) => {
+  Schedules.find(req.query)
+    .populate("admin", "username")
+    .populate("exam", "title")
+    .then((schedule) => {
+      res.json({
+        success: "Exam schedule found successfully",
         payload: schedule,
       });
     })
