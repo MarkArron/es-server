5ef7cad53bc014221094a4b582cc450dea6d2404
MarkArron <135515365+MarkArron@users.noreply.github.com>
2023-08-22 20:45:43 +0800

    appointments
    browse

--- controllers/Appointments.js
+++ controllers/Appointments.js
@@ -10,8 +10,21 @@ exports.save = (req, res) =>
     })
     .catch((err) => res.status(400).json({ error: err.message }));
 
-exports.examinee_appointments = (req, res) =>
-  Appointments.find(req.query)
+exports.browse = (req, res) =>
+  Appointments.find()
+    .populate({
+      path: "schedule",
+      populate: [
+        {
+          path: "exam",
+          select: "title",
+        },
+        {
+          path: "admin",
+          select: "username",
+        },
+      ],
+    })
     .then((appointments) => {
       res.status(201).json({
         success: "Appointments created successfully",
@@ -19,3 +32,38 @@ exports.examinee_appointments = (req, res) =>
       });
     })
     .catch((err) => res.status(400).json({ error: err.message }));
+
+exports.examinee_appointments = (req, res) =>
+  Appointments.find(req.query)
+    .populate({
+      path: "schedule",
+      populate: [
+        {
+          path: "exam",
+          select: "title",
+        },
+        {
+          path: "admin",
+          select: "username",
+        },
+      ],
+    })
+    .then((appointments) => {
+      res.status(201).json({
+        success: "Appointments created successfully",
+        payload: appointments,
+      });
+    })
+    .catch((err) => res.status(400).json({ error: err.message }));
+
+exports.update_status = (req, res) => {
+  const updateStatus = { status: req.body.status };
+  Appointments.findByIdAndUpdate(req.body._id, updateStatus, { new: true })
+    .then((appointment) => {
+      res.json({
+        success: "Appointment's status updated successfully",
+        payload: appointment,
+      });
+    })
+    .catch((err) => res.status(400).json({ error: err.message }));
+};
