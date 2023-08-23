73b65db53102fe5b7bf9a6cab3de3cbe37e2a6f4
MarkArron <135515365+MarkArron@users.noreply.github.com>
2023-08-23 13:58:44 +0800

    Appointments
    save update

--- controllers/Appointments.js
+++ controllers/Appointments.js
@@ -1,14 +1,37 @@
 const Appointments = require("../models/Appointments");
+const Schedules = require("../models/Schedules");
 
-exports.save = (req, res) =>
-  Appointments.create(req.body)
-    .then((appointment) => {
-      res.status(201).json({
-        success: "Appointments created successfully",
-        payload: appointment,
+exports.save = (req, res) => {
+  try {
+    const { schedule } = req.body;
+
+    const selectedSchedule = Schedules.findById(schedule);
+    if (!selectedSchedule) {
+      return res.status(404).json({ error: "Selected schedule not found" });
+    }
+
+    if (selectedSchedule.remainingSlots <= 0) {
+      return res.status(400).json({
+        error: "No available slots for the selected schedule",
+        remainingSlots: 0,
       });
-    })
-    .catch((err) => res.status(400).json({ error: err.message }));
+    }
+
+    const appointment = Appointments.create(req.body);
+
+    // Update remainingSlots in the schedule
+    selectedSchedule.remainingSlots -= 1;
+    selectedSchedule.save();
+
+    res.status(201).json({
+      success: "Appointment created successfully",
+      remainingSlots: selectedSchedule.remainingSlots,
+      payload: appointment,
+    });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
 
 exports.browse = (req, res) =>
   Appointments.find()
