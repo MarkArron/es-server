680ac0253eb972c4834e322581427e5c48699c28
MarkArron <135515365+MarkArron@users.noreply.github.com>
2023-08-25 22:00:17 +0800

    Appointments
    save changes

--- controllers/Appointments.js
+++ controllers/Appointments.js
@@ -2,10 +2,41 @@ const Appointments = require("../models/Appointments");
 const Schedules = require("../models/Schedules");
 
 exports.save = (req, res) => {
+  const { schedule } = req.body;
+
+  Schedules.findById(schedule)
+    .then(async (selectedSchedule) => {
+      if (!selectedSchedule) {
+        return res.status(404).json({ error: "Selected schedule not found" });
+      }
+
+      if (selectedSchedule.remainingSlots <= 0) {
+        return res.status(400).json({
+          error: "No available slots for the selected schedule",
+          remainingSlots: 0,
+        });
+      }
+
+      selectedSchedule.remainingSlots -= 1;
+      await selectedSchedule.save();
+
+      Appointments.create(req.body)
+        .then((appointment) => {
+          res.status(201).json({
+            success: "Appointment created successfully",
+            payload: appointment,
+          });
+        })
+        .catch((err) => res.status(400).json({ error: err.message }));
+    })
+    .catch((err) => res.status(400).json({ error: err.message }));
+};
+
+exports.save = async (req, res) => {
   try {
     const { schedule } = req.body;
 
-    const selectedSchedule = Schedules.findById(schedule);
+    const selectedSchedule = await Schedules.findById(schedule);
     if (!selectedSchedule) {
       return res.status(404).json({ error: "Selected schedule not found" });
     }
@@ -17,11 +48,11 @@ exports.save = (req, res) => {
       });
     }
 
-    const appointment = Appointments.create(req.body);
+    const appointment = await Appointments.create(req.body);
 
     // Update remainingSlots in the schedule
     selectedSchedule.remainingSlots -= 1;
-    selectedSchedule.save();
+    await selectedSchedule.save();
 
     res.status(201).json({
       success: "Appointment created successfully",
@@ -32,7 +63,6 @@ exports.save = (req, res) => {
     res.status(400).json({ error: err.message });
   }
 };
-
 exports.browse = (req, res) =>
   Appointments.find()
     .populate({
