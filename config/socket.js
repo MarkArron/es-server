5cc4317b861e32c5cc88f3c2e551b7f6170991d2
MarkArron <135515365+MarkArron@users.noreply.github.com>
2023-09-19 13:25:57 +0800

    socker minor changes

--- config/socket.js
+++ config/socket.js
@@ -2,15 +2,11 @@ const socket = (io) =>
   io.on("connection", (stream) => {
     console.log(`connection established by: ${stream.id}`);
 
-    stream.on("join_room", (room) => {
-      stream.join(room);
-      // console.log(room);
-    });
+    stream.on("join_room", (room) => stream.join(room));
 
-    stream.on("send_attendance", (data) => {
-      stream.to(data.appointment?.schedule).emit("recieve_attendance", data);
-      // console.log(data.appointment?.schedule);
-    });
+    stream.on("send_attendance", (data) =>
+      io.to(data.appointment?.schedule).emit("recieve_attendance", data)
+    );
 
     stream.on("start_exam", (data) => {
       io.to(data._schedule?._id).emit("start_exam", data);
